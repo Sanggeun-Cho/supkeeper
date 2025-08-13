769529441a4e1704a759f70a330401ae1f2c9d21
SanggeunCho <wtr011124@gmail.com>
2025-08-13 14:30:15 +0900

    [feat] post User

--- src/main/java/com/toy/subkeeper/user/controller/UserController.java
+++ src/main/java/com/toy/subkeeper/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.toy.subkeeper.user.controller;
 import com.toy.subkeeper.DTO.UserDto;
 import com.toy.subkeeper.user.domain.User;
 import com.toy.subkeeper.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +20,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserDto.UserCreateResDto> createUser(@RequestBody UserDto.UserCreateReqDto UserCreateDto){
+    @Operation(summary = "유저 생성",
+            description = "사용자의 이름을 받아 새로운 사용자인 경우 새로 생성, 존재하는 사용자인 경우 해당 사용자의 ID를 반환합니다.<br>" +
+                    "Req : {userName}<br>" +
+                    "Res : {userId, userName}")
+    public ResponseEntity<?> createUser(@RequestBody UserDto.UserCreateReqDto UserCreateDto){
         User savedUser = userService.createUser(UserCreateDto);
         UserDto.UserCreateResDto userDto = new UserDto.UserCreateResDto(savedUser.getId(), savedUser.getUserName());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
+        return ResponseEntity.ok(userDto);
     }
 }
