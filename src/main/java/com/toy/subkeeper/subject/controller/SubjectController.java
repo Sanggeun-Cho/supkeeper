f61864f8de699f02eacde415586aa70f3a1cec5b
SanggeunCho <wtr011124@gmail.com>
2025-08-18 17:28:25 +0900

    [feat] edit subjectController

--- src/main/java/com/toy/subkeeper/subject/controller/SubjectController.java
+++ src/main/java/com/toy/subkeeper/subject/controller/SubjectController.java
@@ -17,12 +17,12 @@ import org.springframework.web.bind.annotation.*;
 public class SubjectController {
     private final SubjectService subjectService;
 
-    @PostMapping("/semId")
+    @PostMapping("/{semId}")
     @Operation(summary = "과목 생성"
             , description = "새로운 과목을 생성하고 이미 존재하는 이름이면 예외 메세지를 반환합니다.<br>" +
             "Req : semId, {subName}<br>" +
             "Res : {subId, subName}")
-    public ResponseEntity<?> createSubject(@RequestParam Long semId, @RequestBody SubjectDto.SubjectCreateReqDto reqDto) {
+    public ResponseEntity<?> createSubject(@PathVariable Long semId, @RequestBody SubjectDto.SubjectCreateReqDto reqDto) {
         try {
             Subject subject = subjectService.createSubject(semId, reqDto);
             SubjectDto.SubjectCreateResDto subjectDto = new SubjectDto.SubjectCreateResDto(subject.getId(), subject.getSubName());
